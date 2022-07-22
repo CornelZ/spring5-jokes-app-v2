1c5dcef7168238a3b19837885d64ef9f8056f996
Cornel Zgardan <cornel.zgardan@modex.tech>
2022-07-22 11:44:36 +0300

    xml configuration

--- src/main/java/guru/springframework/spring5jokesappv2/config/ChuckConfiguration.java
+++ src/main/java/guru/springframework/spring5jokesappv2/config/ChuckConfiguration.java
@@ -1,13 +1,11 @@
 package guru.springframework.spring5jokesappv2.config;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+// @Configuration
 public class ChuckConfiguration {
 
-  @Bean
+  // @Bean
   ChuckNorrisQuotes chuckNorrisQuotes() {
     return new ChuckNorrisQuotes();
   }
